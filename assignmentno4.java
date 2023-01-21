666babdc46c16335731cccba39bff949607ea430
Rajankatre45 <115568241+Rajankatre45@users.noreply.github.com>
2023-01-21 13:20:58 +0530

    Add files via upload

---
+++ assignmentno4.java
@@ -0,0 +1,36 @@
+package in.ineuron.main;
+
+import java.util.*;
+
+class assignmentno4
+{
+	static String removeDuplicate(char str[], int n)
+	{
+		int index = 0;
+		for (int i = 0; i < n; i++)
+		{
+			int j;
+			for (j = 0; j < i; j++)
+			{
+				if (str[i] == str[j])
+				{
+					break;
+				}
+			}
+
+			if (j == i)
+			{
+				str[index++] = str[i];
+			}
+		}
+		return String.valueOf(Arrays.copyOf(str, index));
+	}
+
+	public static void main(String[] args)
+	{
+		char str[] = "no norman normal".toCharArray();
+		int n = str.length;
+		System.out.println(removeDuplicate(str, n));
+	}
+}
+
